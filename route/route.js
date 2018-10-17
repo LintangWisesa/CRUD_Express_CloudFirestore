545c0f587f14beaa1c8d12940a70ad9c8388f2d4
lintangwisesa <lintangwisesa@ymail.com>
2018-10-17 20:28:14 +0700

    express firestore

--- route/route.js
+++ route/route.js
@@ -8,6 +8,18 @@ router.get('/data', (req, res)=>{
     db.settings({
         timestampsInSnapshots: true
     })
+    var allData = []
+    db.collection('karyawan')
+    .orderBy('waktu', 'desc').get()
+    .then(snapshot => {
+        snapshot.forEach((hasil)=>{
+            allData.push(hasil.data())
+            console.log(allData)
+            res.send(allData)
+        })
+    }).catch((error)=>{
+        console.log(error)
+    })
 })
 
 router.post('/data', (req, res)=>{
