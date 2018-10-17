3f112d26d328f153b29210bbd398478d68b8779f
lintangwisesa <lintangwisesa@ymail.com>
2018-10-17 20:26:49 +0700

    express firestore

--- route/route.js
+++ route/route.js
@@ -1,14 +1,19 @@
 var router = require('express').Router()
 var fire = require('./fire')
 var bodyParser = require('body-parser')
+var db = fire.firestore()
 router.use(bodyParser.json())
 
 router.get('/data', (req, res)=>{
-
+    db.settings({
+        timestampsInSnapshots: true
+    })
 })
 
 router.post('/data', (req, res)=>{
-    
+    db.settings({
+        timestampsInSnapshots: true
+    })
 })
 
 module.exports = router
