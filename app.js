c3659aa0b20ea63ae653cfbb5bf8bc27ca2e1990
lintangwisesa <lintangwisesa@ymail.com>
2018-10-17 20:29:47 +0700

    express firestore

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var cors = require('cors')
+var routeSaya = require('./route/route')
 
 var app = express()
 app.use(cors())
 app.use(bodyParser.json())
+app.use(routeSaya)
 
 app.get('/', (req, res)=>{
     res.send('<h1>Express & Firestore</h1>')
