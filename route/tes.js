5922b5ba27dbdfee8e49f9836ef5906b4d53c0dd
lintangwisesa <lintangwisesa@ymail.com>
2018-10-17 20:15:50 +0700

    express firestore

--- route/tes.js
+++ route/tes.js
@@ -6,11 +6,22 @@ db.settings({
 })
 
 // send data to cloud firestore
-db.collection('karyawan').add({
-    nama: 'Budi',
-    usia: 23,
-    kota: 'Bandung',
-    waktu: new Date()
-}).then((error, hasil)=>{
-    console.log(hasil)
+// db.collection('karyawan').add({
+//     nama: 'Budi',
+//     usia: 23,
+//     kota: 'Bandung',
+//     waktu: new Date()
+// }).then((error, hasil)=>{
+//     console.log(hasil)
+// })
+
+// get all data from firestore
+db.collection('karyawan')
+.orderBy('waktu', 'desc').get()
+.then(snapshot => {
+    snapshot.forEach((hasil)=>{
+        console.log(hasil.data())
+    })
+}).catch((error)=>{
+    console.log(error)
 })
